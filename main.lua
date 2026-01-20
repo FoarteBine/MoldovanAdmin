5151c1f78e868aee0864e072ed83d542e2913bb3
FoarteBine <sasabelous332@gmail.com>
2026-01-20 20:20:00 +0200

    Add Filter Bypass button for Chat Back Sign Heck v2
    Added a new button for 'Bring Chat Back Sign Heck v2 Filter Bypass' to load the corresponding script.

--- main.lua
+++ main.lua
@@ -281,6 +281,10 @@ placesSec:Button("Bring Chat Back Sign Heck v2", [[
     loadstring(game:HttpGet('https://raw.githubusercontent.com/FoarteBine/MoldovanAdmin/refs/heads/main/scripts/bring-chat-back-sign-heck-v2.lua'))()
 ]])
 
+placesSec:Button("Bring Chat Back Sign Heck v2 Filter Bypass", [[
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/FoarteBine/MoldovanAdmin/refs/heads/main/scripts/bring-chat-back-sign-heck-filterbypass.lua'))()
+]])
+
 placesSec:Button("Server Admin", [[
     loadstring(game:HttpGet('https://raw.githubusercontent.com/FoarteBine/MoldovanAdmin/refs/heads/main/scripts/server-admin-antiafk.lua'))()
 ]])
